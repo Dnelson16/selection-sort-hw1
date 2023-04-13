00f81e61d618eefe626ed734c8b4ab76a64d93b1
Dylan <dylan.nelson3316@gmail.com>
2023-04-13 16:58:10 -0400

    Selection sort added

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -3,7 +3,31 @@ package src.main.java;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int[] array;
+        array = new int[]{1, 3, 9, 2, 5, 7, 4, 8, 6};
 
+        selectionSort(array);
+
+        for (int i : array) {
+            System.out.print(i);
+        }
+    }
+
+    private static void selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
     }
 }
+
+  
